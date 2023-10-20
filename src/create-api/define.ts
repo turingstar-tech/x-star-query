187be9a03fc499ffc9d4d156993cf0535712de05
zhangjiajun <183728895@qq.com>
2023-10-20 16:33:38 +0800

    fix: 更改类型定义

--- src/create-api/define.ts
+++ src/create-api/define.ts
@@ -1,49 +1,68 @@
 import type { useAntdTable, useRequest } from 'ahooks';
+import type { AntdTableOptions, Data } from 'ahooks/es/useAntdTable/types';
+import type { Options } from 'ahooks/es/useRequest/src/types';
 import type { AxiosRequestConfig, AxiosResponse } from 'axios';
 
-export interface BaseEndpointDefinition<
-  Response extends unknown = void,
-  Request extends unknown = void,
-> {
+export interface BaseEndpointDefinition<Response = void, Request = void> {
   type: string;
   query:
     | string
     | AxiosRequestConfig
     | ((request: Request) => string | AxiosRequestConfig);
   transformResponse?: (response: AxiosResponse) => Response;
+  errorHandlerParams?: unknown;
 }
 
-export interface QueryEndpointDefinition<
-  Response extends unknown = void,
-  Request extends unknown = void,
-> extends BaseEndpointDefinition<Response, Request> {
+export interface QueryEndpointDefinition<Response, Request = void>
+  extends BaseEndpointDefinition<Response, Request> {
   type: 'query';
 }
 
+export type QueryEndpoint<Definition> =
+  Definition extends QueryEndpointDefinition<infer Response, infer Request>
+    ? (
+        request: Request,
+        options?: Options<Response, never>,
+      ) => ReturnType<typeof useRequest<Response, never>>
+    : never;
+
 export interface TableQueryEndpointDefinition<
-  Response extends unknown = void,
-  Request extends unknown = void,
+  Response extends Data,
+  Request = void,
 > extends BaseEndpointDefinition<Response, Request> {
   type: 'tableQuery';
 }
 
-export interface MutateEndpointDefinition<
-  Response extends unknown = void,
-  Request extends unknown = void,
-> extends BaseEndpointDefinition<Response, Request> {
+export type TableQueryEndpoint<Definition> =
+  Definition extends TableQueryEndpointDefinition<infer Response, infer Request>
+    ? (
+        request: Request,
+        options?: AntdTableOptions<Response, never>,
+      ) => ReturnType<typeof useAntdTable<Response, never>>
+    : never;
+
+export interface MutateEndpointDefinition<Response = void, Request = void>
+  extends BaseEndpointDefinition<Response, Request> {
   type: 'mutate';
 }
 
+export type MutateEndpoint<Definition> =
+  Definition extends MutateEndpointDefinition<infer Response, infer Request>
+    ? (
+        options?: Options<Response, [Request]>,
+      ) => ReturnType<typeof useRequest<Response, [Request]>>
+    : never;
+
 export interface EndpointBuilder {
-  query: <Response extends unknown = void, Request extends unknown = void>(
+  query: <Response, Request = void>(
     definition: Omit<QueryEndpointDefinition<Response, Request>, 'type'>,
   ) => QueryEndpointDefinition<Response, Request>;
 
-  tableQuery: <Response extends unknown = void, Request extends unknown = void>(
+  tableQuery: <Response extends Data, Request = void>(
     definition: Omit<TableQueryEndpointDefinition<Response, Request>, 'type'>,
   ) => TableQueryEndpointDefinition<Response, Request>;
 
-  mutate: <Response extends unknown = void, Request extends unknown = void>(
+  mutate: <Response = void, Request = void>(
     definition: Omit<MutateEndpointDefinition<Response, Request>, 'type'>,
   ) => MutateEndpointDefinition<Response, Request>;
 }
@@ -53,19 +72,19 @@ export type EndpointDefinitions = Record<
   BaseEndpointDefinition<any, any>
 >;
 
-export interface ApiConfig<T extends EndpointDefinitions> {
+export interface ApiConfig<Definitions extends EndpointDefinitions> {
   axiosConfig: AxiosRequestConfig;
-  endpoints: (builder: EndpointBuilder) => T;
+  endpoints: (builder: EndpointBuilder) => Definitions;
+  useErrorHandler: (params: unknown) => (error: unknown) => void;
 }
 
-export type CreateApi = <T extends EndpointDefinitions>(
-  config: ApiConfig<T>,
+export type CreateApi = <Definitions extends EndpointDefinitions>(
+  config: ApiConfig<Definitions>,
 ) => {
-  [key in keyof T & string as `use${Capitalize<key>}${Capitalize<
-    T[key]['type']
-  >}`]: ReturnType<
-    T[key]['type'] extends 'tableQuery'
-      ? typeof useAntdTable
-      : typeof useRequest
-  >;
+  [key in keyof Definitions & string as `use${Capitalize<key>}${Capitalize<
+    Definitions[key]['type']
+  >}`]:
+    | QueryEndpoint<Definitions[key]>
+    | TableQueryEndpoint<Definitions[key]>
+    | MutateEndpoint<Definitions[key]>;
 };
--- src/create-api/index.ts
+++ src/create-api/index.ts
@@ -2,3 +2,29 @@ import { CreateApi } from './define';
 
 const createApi: CreateApi = () => {};
 export default createApi;
+
+// const c = createApi({
+//   axiosConfig: {},
+//   endpoints: (builder) => ({
+//     getCll: builder.query<number, string>({
+//       query: '',
+//     }),
+//     setCll: builder.mutate<string, number>({
+//       query: '',
+//     }),
+//     getCllList: builder.tableQuery<{ list: []; total: number }>({
+//       query: '',
+//     }),
+//   }),
+// });
+
+// const { params, data, refreshAsync } = c.useGetCllQuery('1', {
+//   pollingInterval: 1,
+// });
+
+// const { params, runAsync } = c.useSetCllMutate();
+
+// const { params, tableProps, refreshAsync } =
+//   c.useGetCllListTableQuery(undefined);
+
+// c.setErrorHandler;
