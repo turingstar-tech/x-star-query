18e1552c79f366db8933bfc83ccc2843bf994ec9
RegMs If <me@regmsif.cf>
2023-10-25 00:48:59 +0800

    feat: implement query endpoint

--- src/create-api/index.md
+++ src/create-api/index.md
@@ -1,1 +1,52 @@
 # createApi
+
+```tsx
+import { createApi } from 'x-star-query';
+
+const useErrorHandler = (params: { path: string }) => {
+  return (error: any) => console.log(params.path, error);
+};
+
+const { useGetTopContestQuery, useGet404ErrorQuery } = createApi({
+  axiosConfig: { baseURL: 'https://strapi.turingstar.com.cn/api' },
+
+  endpoints: (builder) => ({
+    getTopContest: builder.query<string>({
+      query: { url: '/course-publicities/1' },
+      transformResponse: ({ data }) => data.data,
+    }),
+
+    get404Error: builder.query<string>({
+      query: { url: '/course-publicity/1' },
+      errorHandlerParams: { path: '/course-publicity/1' },
+    }),
+  }),
+
+  useErrorHandler,
+});
+
+export default () => {
+  const { data, loading, refreshAsync } = useGetTopContestQuery(undefined, {
+    pollingInterval: 5000,
+  });
+
+  const { error } = useGet404ErrorQuery();
+
+  return (
+    <>
+      <button onClick={refreshAsync}>重新获取</button>
+      <div
+        style={{
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+        }}
+      >
+        {loading ? '加载中' : JSON.stringify(data)}
+      </div>
+      <hr />
+      <div style={{ color: 'red' }}>{error?.message}</div>
+    </>
+  );
+};
+```
--- src/create-api/index.ts
+++ src/create-api/index.ts
@@ -1,30 +1,74 @@
-import { CreateApi } from './define';
+import { useRequest } from 'ahooks';
+import axios from 'axios';
+import type { CreateApi, QueryEndpoint } from './define';
 
-const createApi: CreateApi = () => {};
+/**
+ * 创建 API
+ *
+ * @param config API 配置
+ * @returns API
+ */
+const createApi: CreateApi = (config) => {
+  /**
+   * Axios 实例
+   */
+  const instance = axios.create(config.axiosConfig);
+
+  /**
+   * 接口定义映射
+   */
+  const endpoints = config.endpoints({
+    query: (definition) => ({ ...definition, type: 'query' }),
+    tableQuery: (definition) => ({ ...definition, type: 'tableQuery' }),
+    mutate: (definition) => ({ ...definition, type: 'mutate' }),
+  });
+
+  return Object.entries(endpoints).reduce<any>((api, [name, definition]) => {
+    const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
+
+    switch (definition.type) {
+      case 'query': {
+        const useEndpoint: QueryEndpoint<typeof definition> = (
+          request,
+          options,
+        ) => {
+          const errorHandler = config.useErrorHandler?.(
+            definition.errorHandlerParams,
+          );
+
+          return useRequest(
+            async () => {
+              const query =
+                typeof definition.query === 'function'
+                  ? definition.query(request)
+                  : definition.query;
+              const config = typeof query === 'string' ? { url: query } : query;
+              const response = await instance.request(config);
+              return definition.transformResponse
+                ? definition.transformResponse(response)
+                : response;
+            },
+            { ...options, onError: errorHandler },
+          );
+        };
+
+        return { ...api, [`use${capitalizedName}Query`]: useEndpoint };
+      }
+
+      case 'tableQuery': {
+        return { ...api };
+      }
+
+      case 'mutate': {
+        return { ...api };
+      }
+
+      default: {
+        const _: never = definition;
+        return _;
+      }
+    }
+  }, {});
+};
+
 export default createApi;
-
-// const c = createApi({
-//   axiosConfig: {},
-//   endpoints: (builder) => ({
-//     getCll: builder.query<number, string>({
-//       query: '',
-//     }),
-//     setCll: builder.mutate<string, number>({
-//       query: '',
-//     }),
-//     getCllList: builder.tableQuery<{ list: []; total: number }>({
-//       query: '',
-//     }),
-//   }),
-// });
-
-// const { params, data, refreshAsync } = c.useGetCllQuery('1', {
-//   pollingInterval: 1,
-// });
-
-// const { params, runAsync } = c.useSetCllMutate();
-
-// const { params, tableProps, refreshAsync } =
-//   c.useGetCllListTableQuery(undefined);
-
-// c.setErrorHandler;
