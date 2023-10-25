b49f7af9bf3754b75dd2aa88e1b717fddd4f4c80
RegMs If <me@regmsif.cf>
2023-10-25 17:31:43 +0800

    fix: configure base and public path

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
-  outputPath: 'docs-dist',
   themeConfig: {
     name: 'x-star-query',
   },
+  base: '/x-star-query/',
+  outputPath: 'docs-dist',
+  publicPath: '/x-star-query/',
 });
