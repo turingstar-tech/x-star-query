58fb860e648d39ca43ed3bd9c68b712c68b5b1e1
RegMs If <me@regmsif.cf>
2023-11-07 20:03:06 +0800

    test: pagination query and table query

--- test/create-api.test.ts
+++ test/create-api.test.ts
@@ -8,10 +8,21 @@ jest.mock('axios', () => ({
   create: () => {
     // 模拟服务器数据
     let store = { hello: 'world!', good: 'night.' };
+    let list = Array.from({ length: 100 }).map((_, id) => ({ id }));
 
     return {
       // 模拟请求方法
-      request: ({ url, params, method = 'GET', data }: AxiosRequestConfig) => {
+      request: async ({
+        url,
+        params,
+        method = 'GET',
+        data,
+      }: AxiosRequestConfig) => {
+        // 模拟服务器延迟
+        await new Promise((resolve) => {
+          setTimeout(resolve, 50);
+        });
+
         // 模拟路由
         switch (url) {
           case '/store': {
@@ -30,6 +41,21 @@ jest.mock('axios', () => ({
             } else if (method === 'POST') {
               store[data.key] = data.value;
               return { data: undefined };
+            }
+            return { data: undefined };
+          }
+
+          case '/list': {
+            if (method === 'GET') {
+              return {
+                data: {
+                  total: list.length,
+                  list: list.slice(
+                    (params.current - 1) * params.pageSize,
+                    params.current * params.pageSize,
+                  ),
+                },
+              };
             }
             return { data: undefined };
           }
@@ -63,14 +89,15 @@ describe('query endpoint', () => {
   });
 
   test('query with params', async () => {
+    const query = jest.fn(({ key }) => ({
+      url: '/store/key',
+      params: { key },
+    }));
+    const options = jest.fn(({ key }) => ({ refreshDeps: [key] }));
+
     const { useGetStoreKeyQuery } = createApi({
       endpoints: (builder) => ({
-        getStoreKey: builder.query<object, { key: string }>({
-          query: ({ key }) => ({ url: '/store/key', params: { key } }),
-          options: ({ key }) => ({
-            refreshDeps: [key],
-          }),
-        }),
+        getStoreKey: builder.query<object, { key: string }>({ query, options }),
       }),
     });
 
@@ -79,21 +106,37 @@ describe('query endpoint', () => {
       { initialProps: { key: 'hello' } },
     );
 
+    expect(query).toBeCalledTimes(1);
+    expect(query.mock.calls[0][0]).toEqual({ key: 'hello' });
+    expect(options).toBeCalledTimes(2);
+    expect(options.mock.calls[0][0]).toEqual({ key: 'hello' });
+    expect(options.mock.calls[1][0]).toEqual({ key: 'hello' });
     expect(result.current.data).toBe(undefined);
     expect(result.current.loading).toBe(true);
 
     await waitForNextUpdate();
 
+    expect(query).toBeCalledTimes(1);
+    expect(options).toBeCalledTimes(3);
+    expect(options.mock.calls[2][0]).toEqual({ key: 'hello' });
     expect(result.current.data).toBe('world!');
     expect(result.current.loading).toBe(false);
 
     rerender({ key: 'good' });
 
+    expect(query).toBeCalledTimes(2);
+    expect(query.mock.calls[1][0]).toEqual({ key: 'good' });
+    expect(options).toBeCalledTimes(5);
+    expect(options.mock.calls[3][0]).toEqual({ key: 'good' });
+    expect(options.mock.calls[4][0]).toEqual({ key: 'good' });
     expect(result.current.data).toBe('world!');
     expect(result.current.loading).toBe(true);
 
     await waitForNextUpdate();
 
+    expect(query).toBeCalledTimes(2);
+    expect(options).toBeCalledTimes(6);
+    expect(options.mock.calls[5][0]).toEqual({ key: 'good' });
     expect(result.current.data).toBe('night.');
     expect(result.current.loading).toBe(false);
   });
@@ -106,13 +149,17 @@ describe('query endpoint', () => {
       return data;
     });
     const errorHandler = jest.fn();
+    const useErrorHandler = jest.fn<(params: any) => any>(() => errorHandler);
 
     const { useThrowErrorQuery } = createApi({
       transformResponse,
       endpoints: (builder) => ({
-        throwError: builder.query<object>({ query: '/error' }),
+        throwError: builder.query<object>({
+          query: { url: '/error' },
+          errorHandlerParams: 'params',
+        }),
       }),
-      useErrorHandler: () => errorHandler,
+      useErrorHandler,
     });
 
     const { result, waitForNextUpdate } = renderHook(() =>
@@ -121,6 +168,9 @@ describe('query endpoint', () => {
 
     expect(transformResponse).toBeCalledTimes(0);
     expect(errorHandler).toBeCalledTimes(0);
+    expect(useErrorHandler).toBeCalledTimes(2);
+    expect(useErrorHandler.mock.calls[0][0]).toBe('params');
+    expect(useErrorHandler.mock.calls[1][0]).toBe('params');
     expect(result.current.error).toBe(undefined);
     expect(result.current.loading).toBe(true);
 
@@ -130,8 +180,158 @@ describe('query endpoint', () => {
     expect(transformResponse.mock.calls[0][0]).toBe(undefined);
     expect(errorHandler).toBeCalledTimes(1);
     expect(errorHandler.mock.calls[0][0]).toEqual(new Error('not found'));
+    expect(useErrorHandler).toBeCalledTimes(3);
+    expect(useErrorHandler.mock.calls[2][0]).toBe('params');
     expect(result.current.error).toEqual(new Error('not found'));
     expect(result.current.loading).toBe(false);
+  });
+});
+
+describe('table query endpoint', () => {
+  test('simple table query', async () => {
+    const { useGetListTableQuery } = createApi({
+      endpoints: (builder) => ({
+        getList: builder.tableQuery<
+          { total: number; list: { id: number }[] },
+          void,
+          { current: number; pageSize: number }
+        >({ query: '/list' }),
+      }),
+    });
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useGetListTableQuery(),
+    );
+
+    expect(result.current.data).toBe(undefined);
+    expect(result.current.loading).toBe(false);
+    expect(result.current.tableProps).toMatchObject({
+      dataSource: [],
+      loading: false,
+      pagination: { current: 1, pageSize: 10, total: 0 },
+    });
+
+    await waitForNextUpdate();
+
+    expect(result.current.data).toBe(undefined);
+    expect(result.current.loading).toBe(true);
+    expect(result.current.tableProps).toMatchObject({
+      dataSource: [],
+      loading: true,
+      pagination: { current: 1, pageSize: 10, total: 0 },
+    });
+
+    await waitForNextUpdate();
+
+    expect(result.current.data).toEqual({
+      total: 100,
+      list: Array.from({ length: 10 }).map((_, id) => ({ id })),
+    });
+    expect(result.current.loading).toBe(false);
+    expect(result.current.tableProps).toMatchObject({
+      dataSource: Array.from({ length: 10 }).map((_, id) => ({ id })),
+      loading: false,
+      pagination: { current: 1, pageSize: 10, total: 100 },
+    });
+
+    act(() => {
+      result.current.tableProps.onChange({ current: 7, pageSize: 16 });
+    });
+
+    expect(result.current.data).toEqual({
+      total: 100,
+      list: Array.from({ length: 10 }).map((_, id) => ({ id })),
+    });
+    expect(result.current.loading).toBe(true);
+    expect(result.current.tableProps).toMatchObject({
+      dataSource: Array.from({ length: 10 }).map((_, id) => ({ id })),
+      loading: true,
+      pagination: { current: 7, pageSize: 16, total: 100 },
+    });
+
+    await waitForNextUpdate();
+
+    expect(result.current.data).toEqual({
+      total: 100,
+      list: Array.from({ length: 4 }).map((_, id) => ({ id: id + 96 })),
+    });
+    expect(result.current.loading).toBe(false);
+    expect(result.current.tableProps).toMatchObject({
+      dataSource: Array.from({ length: 4 }).map((_, id) => ({ id: id + 96 })),
+      loading: false,
+      pagination: { current: 7, pageSize: 16, total: 100 },
+    });
+  });
+});
+
+describe('pagination query endpoint', () => {
+  test('simple pagination query', async () => {
+    const { useGetListPaginationQuery } = createApi({
+      endpoints: (builder) => ({
+        getList: builder.paginationQuery<
+          { total: number; list: { id: number }[] },
+          void,
+          { current: number; pageSize: number }
+        >({ query: '/list' }),
+      }),
+    });
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useGetListPaginationQuery(),
+    );
+
+    expect(result.current.data).toBe(undefined);
+    expect(result.current.loading).toBe(true);
+    expect(result.current.pagination).toMatchObject({
+      current: 1,
+      pageSize: 10,
+      total: 0,
+      totalPage: 0,
+    });
+
+    await waitForNextUpdate();
+
+    expect(result.current.data).toEqual({
+      total: 100,
+      list: Array.from({ length: 10 }).map((_, id) => ({ id })),
+    });
+    expect(result.current.loading).toBe(false);
+    expect(result.current.pagination).toMatchObject({
+      current: 1,
+      pageSize: 10,
+      total: 100,
+      totalPage: 10,
+    });
+
+    act(() => {
+      result.current.pagination.onChange(7, 16);
+    });
+
+    expect(result.current.data).toEqual({
+      total: 100,
+      list: Array.from({ length: 10 }).map((_, id) => ({ id })),
+    });
+    expect(result.current.loading).toBe(true);
+    expect(result.current.pagination).toMatchObject({
+      current: 7,
+      pageSize: 16,
+      total: 100,
+      totalPage: 7,
+    });
+
+    await waitForNextUpdate();
+
+    expect(result.current.data).toEqual({
+      total: 100,
+      list: Array.from({ length: 4 }).map((_, id) => ({ id: id + 96 })),
+    });
+    expect(result.current.loading).toBe(false);
+    expect(result.current.pagination).toMatchObject({
+      current: 7,
+      pageSize: 16,
+      total: 100,
+      totalPage: 7,
+    });
   });
 });
 
@@ -175,5 +375,17 @@ describe('mutate endpoint', () => {
     await waitForNextUpdate();
 
     expect(result.current.get.data).toEqual({ what: 'for?' });
+  });
+});
+
+describe('unknown endpoint', () => {
+  test('simple unknown', () => {
+    const api = createApi({
+      endpoints: () => ({
+        ignore: { type: 'unknown' as any, query: '/unknown' },
+      }),
+    });
+
+    expect(api).toEqual({});
   });
 });
